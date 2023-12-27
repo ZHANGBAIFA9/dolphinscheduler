0ff939f365b31f58ac98f43acf764a7441a07219
ZHANGBAIFA <1350862999@qq.com>
2023-12-27 17:03:44 +0800

    工程依赖调整

--- dolphinscheduler-master/src/main/java/com/afiab/dolphinscheduler/server/master/MasterServer.java
+++ dolphinscheduler-master/src/main/java/com/afiab/dolphinscheduler/server/master/MasterServer.java
@@ -1,6 +1,11 @@
 package com.afiab.dolphinscheduler.server.master;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dolphinscheduler.common.constants.Constants;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @Version 1.0
@@ -8,9 +13,14 @@ import org.apache.dolphinscheduler.common.constants.Constants;
  * @Date 2023/12/14 16:23
  * @Description:
  */
+@SpringBootApplication
+@ComponentScan("com.afiab.dolphinscheduler")
+//@EnableTransactionManagement
+@EnableCaching
+@Slf4j
 public class MasterServer {
     public static void main(String[] args) {
         Thread.currentThread().setName(Constants.THREAD_NAME_MASTER_SERVER);
-//        SpringApplication.run(MasterServer.class);
+        SpringApplication.run(MasterServer.class);
     }
 }
